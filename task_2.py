968107051a8592f6290f69288c0742f5caf99e9a
L1m1rd <postoev36@gmail.com>
2025-09-30 23:07:52 +0400

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -4,51 +4,48 @@ import turtle
 def perform_switch_case(state, t, turn):
     x = round(t.position()[0] / 10)
     y = round(t.position()[1] / 10)
-    num_turns = 1
-    
-    if state == "INIT":
-        if True:
-            state = "DOWN"
-            t.setheading(270)  # Разворот вниз
+    num_turns = 5  # сколько витков
+
+    if state == "UP":
+        t.forward(10)
+        if y >= turn:
+            state = "RIGHT"
+            t.setheading(0)
             return state, turn
         return state, turn
-    if state == "DOWN":
-        t.forward(10)  # Перемещение
 
+    if state == "LEFT":
+        t.forward(10)
+        if x <= -turn:
+            state = "UP"
+            t.setheading(90)
+            return state, turn
+        return state, turn
+
+    if state == "DOWN":
+        t.forward(10)
         if y <= -turn:
-            state = "LEFT"  # Теперь поворачиваем влево вместо правого
-            t.setheading(180)  # Разворот влево
+            state = "LEFT"
+            t.setheading(180)
+            return state, turn
+        return state, turn
+
+    if state == "RIGHT":
+        t.forward(10)
+        if x >= turn:
+            state = "DOWN"
+            t.setheading(270)
+            turn += 1   # <<< добавили шаг спирали
             if turn > num_turns:
                 state = "STOP"
             return state, turn
         return state, turn
-    if state == "LEFT":
-        t.forward(10)  # Перемещение
 
-        if x <= -turn:
-            state = "UP"  # После левого поворачиваем вверх
-            t.setheading(90)  # Разворот вверх
-            return state, turn
+    if state == "INIT":
+        state = "RIGHT"
+        t.setheading(0)
         return state, turn
-    if state == "UP":
-        t.forward(10)  # Перемещение
 
-        if y >= turn:
-            state = "RIGHT"  # После верхнего поворачиваем вправо
-            t.setheading(0)  # Разворот вправо
-            return state, turn
-        return state, turn
-    if state == "RIGHT":
-        t.forward(10)  # Перемещение
-
-        if x >= turn:
-            state = "DOWN"
-            t.setheading(270)  # Разворот вниз
-            turn = turn + 1  # Начало нового витка
-            return state, turn
-        return state, turn
-    if state == "STOP":
-        return state, turn
     return state, turn
 
 
@@ -58,12 +55,13 @@ def draw():
     curr_state = start_state
     t = turtle.Turtle()
     t.speed(0)
-    turn = 0
+    turn = 1
 
     while curr_state != end_state:
         curr_state, turn = perform_switch_case(curr_state, t, turn)
+
     turtle.done()
 
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     draw()
